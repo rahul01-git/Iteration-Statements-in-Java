31332701946670cfc1c90af07c90a21d1017f75a
RahulNeupane <rahulneupane0090@gmail.com>
2023-02-19 10:51:50 +0545

    iteration statement in java

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main{
+    public static void main(String[] args) {
+        // // for loop 
+        // for(int money = 0; money< 100; money ++){
+        //     System.out.println("Money" + money);
+        // }
+
+        // int money = 0;
+        // // while loop 
+        // while(money<100){
+        //     System.out.println("Money " + money);
+        //     money++;
+        // }
+
+        // // dowhile loop 
+        // do{
+        //     System.out.println("Money  " + money);
+        //     money++;
+        // }while(money<100);
+
+        int[] age = {40,50,60};
+        for(int element:age){
+            System.out.println("age >> " + element);
+        }
+        
+    }
+}
